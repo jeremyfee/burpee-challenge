6902811f6759553ea38d1568913a7810fd9b9e39
Jeremy Fee <jeremyfee@gmail.com>
2022-09-26 22:48:47 -0600

    Add start day

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Burpees } from "./Burpees";
 function App() {
   return (
     <div className="App">
-      <Burpees numDays={100} start={new Date("2022-09-23")} />
+      <Burpees numDays={100} start={new Date(2022, 8, 23)} />
     </div>
   );
 }
--- src/Burpees.tsx
+++ src/Burpees.tsx
@@ -1,15 +1,52 @@
+const LOCALE = "en-nz";
+const OPTIONS = {
+  weekday: "long",
+  year: "numeric",
+  month: "short",
+  day: "numeric",
+};
+
 export interface BurpeesProps {
   numDays: number;
   start: Date;
 }
 
+
 export function Burpees(props: BurpeesProps) {
   const { numDays, start } = props;
   const now = new Date();
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  const end = new Date(start.getTime() + numDays * 86400000);
   const dayNumber =
     Math.round((today.getTime() - start.getTime()) / 86400000) + 1;
   const completed = total(1, Math.min(dayNumber + 1, numDays + 1));
+  const remaining = total(dayNumber + 1, numDays + 1);
+
+  return (
+    <>
+      <h1>Burpee Challenge</h1>
+      <small>
+        Starting {start.toLocaleString(LOCALE, OPTIONS)},
+        +1 burpee each day for {numDays} days.<br/>
+      </small>
+
+      <dl>
+        <dt>
+          Today (
+          {today.toLocaleDateString(LOCALE, OPTIONS)}
+          )
+        </dt>
+        <dd>{dayNumber > numDays ? "&ndash;" : dayNumber}</dd>
+        <dt>Completed (includes Today)</dt>
+        <dd>{completed}</dd>
+        <dt>Remaining</dt>
+        <dd>{remaining}</dd>
+      </dl>
+      <p>
+        If you miss a day, do <em>Completed</em> to buy back in.
+      </p>
+    </>
+  );
 
   function total(first: number, last: number) {
     var sum = 0;
@@ -18,31 +55,4 @@ export function Burpees(props: BurpeesProps) {
     }
     return sum;
   }
-
-  return (
-    <>
-      <h1>Burpee Challenge</h1>
-
-      <dl>
-        <dt>
-          Today (
-          {today.toLocaleString("en-nz", {
-            weekday: "long",
-            year: "numeric",
-            month: "short",
-            day: "numeric",
-          })}
-          )
-        </dt>
-        <dd>{dayNumber > numDays ? "&ndash;" : dayNumber}</dd>
-        <dt>Completed (includes Today)</dt>
-        <dd>{completed}</dd>
-        <dt>Remaining</dt>
-        <dd>{total(dayNumber + 1, numDays + 1)}</dd>
-      </dl>
-      <p>
-        If you miss a day, do <em>Completed</em> to buy back in
-      </p>
-    </>
-  );
 }
